db64c86d30f894901b062bcb9acd9a60c465b735
kongfuchen <18852894936@163.com>
2020-05-25 09:49:24 +0100

    kfc

--- dht.py
+++ dht.py
@@ -14,8 +14,8 @@ GPIO.setup(GPIO_IR,GPIO.IN)
 GPIO.setup(GPIO_MQ_2,GPIO.IN)
 #humidity, temperature = Adafruit_DHT.read_retry(sensor, gpio)
 #print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
-url = "http://api.heclouds.com/devices/597808637/datapoints"
-API_KEY = 'pUMeUM=pxUhsNZcyLSOkYqNexKA='
+url = "http://api.heclouds.com/devices/599461350/datapoints"
+API_KEY = 'DC1E5ISmakdYV7fzXQAG8iXL08U='
 headers = {'api-key':API_KEY}
 while True:
 	humidity, temperature = Adafruit_DHT.read_retry(sensor, GPIO_DHT)
@@ -28,14 +28,16 @@ while True:
 		request.get_method = lambda:'POST' # 设置HTTP的访问方式 
 		request = urllib2.urlopen(request)
 		print request.read()
-		values = { "datastreams": [ { "id": "hum", "datapoints": [{"value":humidity}]}]}
+		values = { "datastreams": [ { "id": "humi", "datapoints": [{"value":humidity}]}]}
 		jdata = json.dumps(values)
 		request = urllib2.Request(url,jdata)
         	request.add_header('api-key',API_KEY)
         	request.get_metmod = lambda:'POST'
         	request = urllib2.urlopen(request)
 		print request.read()
-		if GPIO.input(GPIO_IR)==GPIO.HIGH:
+		str=time.strftime('%Y.%m.%d %H:%M:%S ',time.localtime(time.time()))
+		h=int(str[10,13])
+		if GPIO.input(GPIO_IR)==GPIO.HIGH and h>9 and h<17:
 			IR=1
 		else: IR=0
 		values = { "datastreams": [ { "id": "ir", "datapoints": [{"value":IR}]}]}
